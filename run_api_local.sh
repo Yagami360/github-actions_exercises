8384dbd1c7d1f11207cbf6bad6dce62bc61d159a
Yagami360 <y034112@gmail.com>
2021-01-31 15:54:16 +0900

    a

--- run_api_local.sh
+++ run_api_local.sh
@@ -6,7 +6,6 @@ docker-compose -f api/docker-compose.yml up -d
 #docker exec -it api-sample-container bash
 
 sleep 1
-cd api
-python request.py --host 0.0.0.0 --port 5000 --request_value 1 --debug
+python api/request.py --host 0.0.0.0 --port 5000 --request_value 1 --debug
 sleep 5
-python request.py --host 0.0.0.0 --port 5000 --request_value 0 --debug
+python api/request.py --host 0.0.0.0 --port 5000 --request_value 0 --debug
